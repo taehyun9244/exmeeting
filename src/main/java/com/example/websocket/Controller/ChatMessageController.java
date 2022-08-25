cd07a913d92a2a725421a3bd951303fe5ce63392
taehyun9244 <85472257+taehyun9244@users.noreply.github.com>
2022-08-25 23:49:24 +0900

    view restart

--- src/main/java/com/example/websocket/Controller/ChatMessageController.java
+++ src/main/java/com/example/websocket/Controller/ChatMessageController.java
@@ -1,6 +1,6 @@
 package com.example.websocket.Controller;
 
-import com.example.websocket.dto.ChatMessageDTO;
+import com.example.websocket.dto.ChatMessageDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -13,13 +13,13 @@ public class ChatMessageController {
     private final SimpMessagingTemplate template;
 
     @MessageMapping(value = "/chat/enter")
-    public void enter(ChatMessageDTO message){
+    public void enter(ChatMessageDto message){
         message.setMessage(message.getWriter() + "님이 채팅방에 참여하였습니다.");
         template.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
     }
 
     @MessageMapping(value = "/chat/message")
-    public void message(ChatMessageDTO message){
+    public void message(ChatMessageDto message){
         template.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
     }
 }
--- src/main/java/com/example/websocket/dto/ChatMessageDto.java
+++ src/main/java/com/example/websocket/dto/ChatMessageDto.java
@@ -3,7 +3,7 @@ package com.example.websocket.dto;
 import lombok.Data;
 
 @Data
-public class ChatMessageDTO {
+public class ChatMessageDto {
     private String roomId;
     private String writer;
     private String message;
